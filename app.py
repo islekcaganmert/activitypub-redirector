68831376c2d3aaed27695e64e91456233276de20
Çağan Mert İŞLEK <islekcaganmert@gmail.com>
2024-01-06 21:42:30 +0300

    Accept header fix

--- app.py
+++ app.py
@@ -14,12 +14,17 @@ def get_aas(domain):
 
 @app.route('/user/<username>.json')
 def instance_user(username):
-    webfinger = requests.get(f"https://{get_aas(username.split('@')[2])}/.well-known/webfinger?resource=acct:{username.removeprefix('@')}").json()
+    webfinger = requests.get(
+        f"https://{get_aas(username.split('@')[2])}/.well-known/webfinger?resource=acct:{username.removeprefix('@')}",
+        headers={
+            'Accept': 'application/jrd+json'
+        }
+    ).json()
     link = ''
     for i in webfinger['links']:
         if i['type'] == 'application/activity+json':
             link = i['href']
-    return requests.get(link).json()
+    return requests.get(link, headers={'Accept': 'application/activity+json'}).json()
 
 @app.route('/post/<id>.json')
 def instance_post(id):
